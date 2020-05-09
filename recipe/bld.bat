fe228b139ed1ad032975bf3235f81e7c365a0eeb
Antony Lee <anntzer.lee@gmail.com>
2020-05-09 20:57:01 +0200

    Add single-precision library.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,54 @@
-mkdir build
-cd build
+mkdir build-single-static
+cd build-single-static
 
-cmake -G "NMake Makefiles" ^
+cmake -Wno-dev -G "NMake Makefiles" ^
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+  -DBUILD_SHARED_LIBS:BOOL=OFF ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+  -DUSE_BLAS:BOOL=OFF ^
+  -DCMAKE_C_FLAGS=/D__cminpack_float__ ^
+  -DCMAKE_RELWITHDEBINFO_POSTFIX=s_s ^
+  .. || goto :eof
+
+cmake --build . --config Release --target install || goto :eof
+cd ..
+
+mkdir build-double-static
+cd build-double-static
+
+cmake -Wno-dev -G "NMake Makefiles" ^
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+  -DBUILD_SHARED_LIBS:BOOL=OFF ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+  -DUSE_BLAS:BOOL=OFF ^
+  -DCMAKE_RELWITHDEBINFO_POSTFIX=_s ^
+  .. || goto :eof
+
+cmake --build . --config Release --target install || goto :eof
+cd ..
+
+mkdir build-single-shared
+cd build-single-shared
+
+cmake -Wno-dev -G "NMake Makefiles" ^
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+  -DBUILD_SHARED_LIBS:BOOL=ON ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+  -DUSE_BLAS:BOOL=OFF ^
+  -DCMAKE_C_FLAGS=/D__cminpack_float__ ^
+  -DCMAKE_RELWITHDEBINFO_POSTFIX=s ^
+  .. || goto :eof
+
+cmake --build . --config Release --target install || goto :eof
+cd ..
+
+mkdir build-double-shared
+cd build-double-shared
+
+cmake -Wno-dev -G "NMake Makefiles" ^
   -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
   -DBUILD_SHARED_LIBS:BOOL=ON ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
@@ -10,6 +57,8 @@ cmake -G "NMake Makefiles" ^
   .. || goto :eof
 
 cmake --build . --config Release --target install || goto :eof
+cd ..
 
-unix2dos ../examples/ref/*.ref
-ctest --output-on-failure
+rem cd build-double-shared
+rem unix2dos ../examples/ref/*.ref
+rem ctest --output-on-failure
